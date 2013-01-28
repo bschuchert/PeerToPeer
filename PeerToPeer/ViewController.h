c13e2511a5165b8c2c9e60f121ef57db19cb0eab
Bobby Schuchert <bobby@liquix.com>
2013-01-27 20:35:01 -0500

    Changed to iPhone only and added support for Emoji in message.

--- PeerToPeer/ViewController.h
+++ PeerToPeer/ViewController.h
@@ -7,7 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <GameKit/GameKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <GKSessionDelegate, GKPeerPickerControllerDelegate>
+
+@property (nonatomic, strong) GKSession *session;
+@property (nonatomic, strong) GKPeerPickerController *picker;
+@property (nonatomic, strong) NSMutableArray *peers;
+
+@property (nonatomic, weak) IBOutlet UIButton *connectButton;
+@property (nonatomic, weak) IBOutlet UIButton *sendMessageButton;
+
+- (IBAction) connectToPeers:(id) sender;
+- (IBAction) sendMessageButtonPressed:(id) sender;
+
 
 @end
--- PeerToPeer/ViewController.m
+++ PeerToPeer/ViewController.m
@@ -17,13 +17,116 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    _picker = [[GKPeerPickerController alloc] init];
+    _picker.delegate = self;
+    
+    //There are 2 modes of connection type
+    // - GKPeerPickerConnectionTypeNearby via BlueTooth
+    // - GKPeerPickerConnectionTypeOnline via Internet
+    // We will use Bluetooth Connectivity for this example
+    
+    _picker.connectionTypesMask = GKPeerPickerConnectionTypeNearby;
+    _peers=[[NSMutableArray alloc] init];
+    
+    _connectButton.hidden = NO;
+    _sendMessageButton.hidden = YES;
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
+
+// Connect to other peers by displayign the GKPeerPicker
+- (IBAction) connectToPeers:(id) sender {
+    [_picker show];
+}
+
+
+#pragma mark - Message Entry Alertview
+- (IBAction)sendMessageButtonPressed:(id)sender {
+    UIAlertView *messageEntry = [[UIAlertView alloc] initWithTitle:@"Enter Message Text"
+                                                      message:nil
+                                                     delegate:self
+                                            cancelButtonTitle:@"Cancel"
+                                            otherButtonTitles:@"Send", nil];
+    [messageEntry setDelegate:self];
+    [messageEntry setAlertViewStyle:UIAlertViewStylePlainTextInput];
+    [messageEntry show];
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    NSString *title = [alertView buttonTitleAtIndex:buttonIndex];
+    
+    if([title isEqualToString:@"Send"])
+    {
+        UITextField *textField = [alertView textFieldAtIndex:0];
+        NSString *message = textField.text;
+        [_session sendData:[message dataUsingEncoding: NSNonLossyASCIIStringEncoding] toPeers:_peers withDataMode:GKSendDataReliable error:nil];
+        
+    }
+}
+
+
+
+#pragma mark - GKPeerPickerControllerDelegate
+
+// This creates a unique Connection Type for this particular applictaion
+- (GKSession *)peerPickerController:(GKPeerPickerController *)picker sessionForConnectionType:(GKPeerPickerConnectionType)type {
+    // Create a session with a unique session ID - displayName:nil = Takes the iPhone Name
+    GKSession* session = [[GKSession alloc] initWithSessionID:@"com.mycompany.peertopeer" displayName:nil sessionMode:GKSessionModePeer];
+    return session;
+}
+
+// Tells us that the peer was connected
+- (void)peerPickerController:(GKPeerPickerController *)picker didConnectPeer:(NSString *)peerID toSession:(GKSession *)session {
+    
+    // Get the session and assign it locally
+    _session = session;
+    _session.delegate = self;
+    
+    //dismiss the picker
+    _picker.delegate = nil;
+    [_picker dismiss];
+}
+
+// Function to receive data when sent from peer
+- (void)receiveData:(NSData *)data fromPeer:(NSString *)peer inSession: (GKSession *)session context:(void *)context
+{
+    //Convert received NSData to NSString to display
+    NSString *incomingMessage = [[NSString alloc] initWithData:data encoding:NSNonLossyASCIIStringEncoding];
+    
+    //Display the message as a UIAlertView
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Message Received" message:incomingMessage delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [alert show];
+}
+
+#pragma mark -
+#pragma mark GKSessionDelegate
+
+- (void)session:(GKSession *)session peer:(NSString *)peerID didChangeState:(GKPeerConnectionState)state{
+    
+    if(state == GKPeerStateConnected){
+        
+        // Add the peer to the Array
+        [_peers addObject:peerID];
+        
+        NSString *str = [NSString stringWithFormat:@"Connected with %@",[session displayNameForPeer:peerID]];
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Connected" message:str delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+        
+        // Used to acknowledge that we will be sending data
+        [session setDataReceiveHandler:self withContext:nil];
+    
+        _connectButton.hidden = YES;
+        _sendMessageButton.hidden = NO;
+        
+    }
+    
+}
+
 
 @end
